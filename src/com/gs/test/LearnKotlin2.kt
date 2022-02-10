9fbd802468fda1f8de13c312601c083b4efb4c2a
GsBu <1129652584@qq.com>
2022-02-10 20:19:45 +0800

    Kotlin函数，创建集合

---
+++ src/com/gs/test/LearnKotlin2.kt
@@ -0,0 +1,24 @@
+package com.gs.first.com.gs.test
+
+fun main(args: Array<String>){
+    val set = setOf(1, 2, 3, 4, 5, 6, 88)
+    val list = listOf("a", "b", "c")
+    val map = mapOf(1 to "aa", 3 to "sd", 77 to "sgrger", 4 to "fore")
+    for (num in set.withIndex()){//Set也可以调用withIndex
+        print("$num, ")
+    }
+    println("\n ${set.javaClass}, ${list.javaClass}, ${map.javaClass}")
+    println("${list}最后一个数据=${list.last()}，${set}最大值=${set.max()}")
+
+}
+
+fun <T>joinToString(collection: Collection<T>, separator: String, prefix: String, postfix: String): String{
+    val result = StringBuilder(prefix)
+
+    for ((index, element) in collection.withIndex()){
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
