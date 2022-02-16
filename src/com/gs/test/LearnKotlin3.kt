1c5e0269fe9055790e8e4eae2fcfe3af4b4d6e52
GsBu <1129652584@qq.com>
2022-02-16 15:35:01 +0800

    Kotlin类，实现接口、接口方法默认实现

--- src/com/gs/test/LearnKotlin3.kt
+++ src/com/gs/test/LearnKotlin3.kt
@@ -3,14 +3,31 @@ package com.gs.first.com.gs.test
 fun main(args: Array<String>){
     println("类、对象、接口")
     MyButton().click()
+    MyButton().showOff()
 }
 
 interface Clickable{
     fun click()
+    fun showOff(){
+        println("默认实现111")
+    }
 }
 
-class MyButton : Clickable{
+interface Clickable2{
+    fun click()
+    fun showOff(){
+        println("默认实现222")
+    }
+}
+
+class MyButton : Clickable, Clickable2{
     override fun click() {
         println("自定义按钮")
+    }
+
+    override fun showOff() {
+        super<Clickable2>.showOff()
+        super<Clickable>.showOff()
+        super<Clickable2>.showOff()
     }
 }
