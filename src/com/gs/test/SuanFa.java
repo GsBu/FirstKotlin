55c1016cb5acbd1c7d4e91b9578eb339adf5cc1b
GsBu <1129652584@qq.com>
2022-03-09 19:02:30 +0800

    1、算法练习

--- src/com/gs/test/SuanFa.java
+++ src/com/gs/test/SuanFa.java
@@ -721,4 +721,28 @@ public class SuanFa {
         }
         return true;
     }
+
+    //135. 分发糖果
+    public int candy(int[] ratings) {
+        int[] left = new int[ratings.length];
+        for (int i = 0; i < left.length; i++) {
+            if (i > 0 && ratings[i] > ratings[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            } else {
+                left[i] = 1;
+            }
+        }
+
+        int right = 0;
+        int ret = 0;
+        for (int i = ratings.length - 1; i >= 0; i--) {
+            if (i < ratings.length - 1 && ratings[i] > ratings[i + 1]) {
+                right++;
+            } else {
+                right = 1;
+            }
+            ret += Math.max(right, left[i]);
+        }
+        return ret;
+    }
 }
