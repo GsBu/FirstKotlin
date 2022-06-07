da7263bf4e1a15f3076e3d52026f820a6375bea6
Gs <1129652584@qq.com>
2022-06-07 12:33:00 +0800

    判断链表有环

--- src/com/gs/test/SuanFa.java
+++ src/com/gs/test/SuanFa.java
@@ -1050,4 +1050,19 @@ public class SuanFa {
             }
         }
     }
+
+    //判断链表有环
+    public static boolean isCycle(ListNode listNode) {
+        ListNode p1 = listNode;
+        ListNode p2 = listNode;
+
+        while (p2 != null && p2.next != null){
+            p1 = p1.next;
+            p2 = p2.next.next;
+            if(p1 == p2){
+                return true;
+            }
+        }
+        return false;
+    }
 }
